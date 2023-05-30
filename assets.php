33ab3df08240b143190bcfa300a111389363df99
efrain <efrain.akl@gmail.com>
2023-05-30 15:24:53 -0400

    datatable

--- assets.php
+++ assets.php
@@ -1,9 +1,11 @@
 
  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="../assets/css/styles.min.css">
+<link rel="stylesheet" href="../assets/css/jquery.dataTables.min.css">
 
 <script src="../assets/js/jquery-3.6.0.min.js"></script>
 <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/js/jquery.dataTables.min.js"></script>
 
